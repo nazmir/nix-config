51e5fd830f70fe514db197bbd29cad7c6b4836f2
Naz Mir <mirnaz.hussain@gmail.com>
2023-08-17 18:36:36 +0530

    Added architecture to flake

--- flake.nix
+++ flake.nix
@@ -23,10 +23,19 @@
     nixosConfigurations = {
       # FIXME replace with your hostname
       mir-nixos-thinkpad = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        system = "x86_64-linux";
+				specialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main nixos configuration file <
         modules = [ ./nixos/configuration.nix ];
       };
+
+			mir-nixos-mbp = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+				specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main nixos configuration file <
+        modules = [ ./nixos/configuration.nix ];
+      };
+
     };
 
     # Standalone home-manager configuration entrypoint
@@ -34,11 +43,20 @@
     homeConfigurations = {
       # FIXME replace with your username@hostname
       "mir@mir-nixos-thinkpad" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+				#system = "x86_64-linux";        
+				pkgs = nixpkgs.legacyPackages."x86_64-linux"; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
         modules = [ ./home-manager/home.nix ];
       };
+
+			"mir@mir-nixos-mbp" = home-manager.lib.homeManagerConfiguration {
+       	#system = "aarch64-linux";
+			 	pkgs = nixpkgs.legacyPackages."aarch64-linux"; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main home-manager configuration file <
+        modules = [ ./home-manager/home.nix ];
+      };		
     };
   };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,6 @@
     fishPlugins.grc
 		fishPlugins.colored-man-pages
 		git
-		emacs
 		vim
 	];
 
@@ -46,6 +45,15 @@
 	    { name = "fishplugin-grc-unstable"; src = pkgs.fishPlugins.grc.src; }
 		];
 	};
+
+ 	programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-gtk;  # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+    extraConfig = ''
+      (setq standard-indent 2)
+    '';
+  };
+
 
 	dconf.settings = {
 	  "org/virt-manager/virt-manager/connections" = {

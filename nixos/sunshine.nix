f7297670773a3db25e7e99ff988d932fe9e51355
Naz Mir <mirnaz.hussain@gmail.com>
2024-11-08 23:18:58 +0530

    System Update

--- nixos/sunshine.nix
+++ nixos/sunshine.nix
@@ -15,7 +15,7 @@
 
 	networking.firewall = {
 		enable = true;
-		allowedTCPPorts = [ 47984 47989 48010 ];
+		allowedTCPPorts = [ 47990 47984 47989 48010 ];
 		allowedUDPPorts = [ 47998 47999 48000 48002 48010 ];		
 	};
 

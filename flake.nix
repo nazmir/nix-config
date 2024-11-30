95d2aa1fa85db98efb9a6ffa21d232eb5d7ddb1e
Naz Mir <nazmir@users.noreply.github.com>
2024-11-30 21:04:31 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgspkgs.nixVersions.unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
